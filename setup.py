1810d1bea312ce4a42b8ee796916773fcff6397f
Michael Gordon <gordon@castor>
2018-08-27 11:22:00 -0700

    fixed numpy dependency

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(name='liccy',
       version='0.1',
@@ -9,5 +10,6 @@ setup(name='liccy',
       author_email='msgordon.astro@gmail.com',
       cmdclass = {'build_ext': build_ext},
       ext_modules = [Extension("liccy",
-                               ["liccy.pyx"])]
+                               ["liccy.pyx"]),
+                     include_dirs=[numpy.get_include()]]
 )
